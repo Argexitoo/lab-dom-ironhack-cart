24503c1a5f01ec2f5626f4afa6af12ce2f2e7ef0
Argexitoo <argexitoo@gmail.com>
2021-11-20 08:57:26 +0100

    pendiente iteracion 3

--- js/index.js
+++ js/index.js
@@ -1,23 +1,30 @@
-// ITERATION 1
-
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector(".price span").innerText;
+  const quantity = product.querySelector(".quantity input").value;
+  const subTotalPrice = price * quantity;
+  const total = product.querySelector(".subtotal span").innerText = subTotalPrice;
+  //return total;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const all = document.getElementsByClassName("product");
+  let total = 0;
+  for(i = 0; i < all.length; i++){
+    total += updateSubtotal(all[i])
+  }
 
   // ITERATION 3
-  //... your code goes here
+  let calculateTotal = document.querySelector("#total-value span");
+  console.log(total)
+  
+  
 }
 
 // ITERATION 4
